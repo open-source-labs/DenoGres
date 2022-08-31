c7a012d384e4270cb86ba3ce10f625394d138159
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-08-31 11:56:18 -0700

    navbar

--- application/fresh.gen.ts
+++ application/fresh.gen.ts
@@ -4,14 +4,16 @@
 
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
+import * as $2 from "./routes/browser.tsx";
+import * as $3 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/browser.tsx": $2,
+    "./routes/index.tsx": $3,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ application/routes/browser.tsx
@@ -0,0 +1,8 @@
+/** @jsx h */
+
+import { h } from "preact";
+import { tw } from "@twind";
+
+export default function Home() {
+  return <div>Hello</div>;
+}
