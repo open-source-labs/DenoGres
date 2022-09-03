4aa36f88063304ef4754f3badfdd5f6eeab378c3
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-09-03 08:52:12 -0700

    webview

---
+++ webview/webview.ts
@@ -0,0 +1,13 @@
+import { SizeHint, Webview } from "https://deno.land/x/webview/mod.ts";
+
+const webview = new Webview();
+
+webview.size = {
+  width: 1024,
+  height: 768,
+  hint: SizeHint.FIXED,
+};
+
+webview.title = "Denogres 2.0";
+webview.navigate("http://localhost:8000");
+webview.run();
