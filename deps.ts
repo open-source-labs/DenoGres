4b9075560bab80ae2e7b879d2350c1c4634a3a03
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-09-03 20:55:48 -0700

    deps.ts

---
+++ deps.ts
@@ -0,0 +1,5 @@
+export { config, parse } from "https://deno.land/std@0.148.0/dotenv/mod.ts"; // mod.ts
+
+export { Pool, PoolClient } from "https://deno.land/x/postgres/mod.ts"; // Db.ts
+
+export { ensureDir } from "https://deno.land/std/fs/ensure_dir.ts"; // init.ts
