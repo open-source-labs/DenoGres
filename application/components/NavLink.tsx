71b98101cbbf99b1808945453f0bfe1626a72f10
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-09-05 16:44:52 -0700

    nav-bar corrections

--- application/components/NavLink.tsx
+++ application/components/NavLink.tsx
@@ -2,13 +2,20 @@
 import { h } from "preact";
 import { tw } from "@twind";
 
-function NavLink({ children }: any) {
+function NavLink(
+  props: { href: string; active: string; svg: h.JSX.Element },
+) {
+  const { href, active, svg } = props;
   return (
-    <div
-      className={tw`bg-gray-200 mb-3 p-2 rounded hover:bg-gray-500 hover:text-white`}
-    >
-      {children}
-    </div>
+    <a href={href}>
+      <div
+        className={tw`${
+          href === active ? "bg-gray-500 text-white" : "bg-gray-200"
+        } mb-3 p-2 rounded hover:bg-gray-500 hover:text-white transition-300`}
+      >
+        {svg}
+      </div>
+    </a>
   );
 }
 export default NavLink;
