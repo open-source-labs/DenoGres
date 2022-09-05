1d29efc8741f1d3c5f255926f6696f1a81312a0f
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-09-04 22:40:43 -0700

    added CLI cmd

--- mod.ts
+++ mod.ts
@@ -1,37 +1,43 @@
-import { parse, config } from './deps.ts'
+import { config, parse } from "./deps.ts";
 
 import "https://deno.land/x/dotenv/load.ts";
 
-import { init } from './src/functions/init.ts'
-import { sync } from './src/functions/sync.ts'
-import { dbPull } from './src/functions/dbPull.ts'
+import { init } from "./src/functions/init.ts";
+import { sync } from "./src/functions/sync.ts";
+import { dbPull } from "./src/functions/dbPull.ts";
 
-switch(Deno.args[0]) {
-    case '--init':
-        init();
-        break;
+switch (Deno.args[0]) {
+  case "--init":
+    init();
+    break;
 
-    case '--db-pull': {// introspection begins
-        const envVar = parse(await Deno.readTextFile('./.env')); // Get DB_URI
+  case "--db-pull": { // introspection begins
+    const envVar = parse(await Deno.readTextFile("./.env")); // Get DB_URI
 
-        if(envVar.DATABASE_URI === "") {
-            console.log('Please enter a valid DATABASE_URI value in .env')
-        } else {
-          dbPull();
-        }
-        break;
+    if (envVar.DATABASE_URI === "") {
+      console.log("Please enter a valid DATABASE_URI value in .env");
+    } else {
+      dbPull();
     }
-    case '--db-sync': {
-      Deno.args[1] === '-x' ? sync(true) : sync();
-      break;
-    } case '-h': {
-      console.log(displayHelpMsg());
-      break;
-    } case '--help': {
-      console.log(displayHelpMsg());
-      break;
-    }
-    default:
+    break;
+  }
+  case "--db-sync": {
+    Deno.args[1] === "-x" ? sync(true) : sync();
+    break;
+  }
+  case "-h": {
+    console.log(displayHelpMsg());
+    break;
+  }
+  case "--help": {
+    console.log(displayHelpMsg());
+    break;
+  }
+  case "--gui": {
+    Deno.run({ cmd: ["deno", "run", "-A", "webview_script.ts"] });
+    break;
+  }
+  default:
 }
 
 function displayHelpMsg() {
@@ -40,5 +46,5 @@ function displayHelpMsg() {
 --db-pull: Introspect database and create and populate model.ts file`;
 }
 
-export { Model } from './src/class/Model.ts'
-export { manyToMany } from './src/class/Model.ts'
+export { Model } from "./src/class/Model.ts";
+export { manyToMany } from "./src/class/Model.ts";
