c36b1252adb1503bfba08cbcae713d992df45707
Henry Park <codedenma@gmail.com>
2022-09-06 03:09:26 -0700

    seed

--- Test/seed.ts
+++ Test/seed.ts
@@ -5,10 +5,9 @@ import {
   assertStrictEquals,
   assertThrows,
 } from "https://deno.land/std@0.150.0/testing/asserts.ts";
-// import { Model } from "../src/class/Model.ts";
-import * as denogres from "../models/model.ts";
+import { Model } from "../src/class/Model.ts";
 import { ConnectDb, DisconnectDb } from "../src/functions/Db.ts";
-import { Person } from "../models/model.ts";
+import * as denogres from "../models/model.ts";
 
 Deno.test(async function helloWorld3() {
   const db = await ConnectDb(
@@ -16,55 +15,49 @@ Deno.test(async function helloWorld3() {
     // "postgres://obdwuryp:EcSMdYz0mPXGgiQSD4_8vLEYPjkHOJ5e@heffalump.db.elephantsql.com/obdwuryp",
   );
 
-  // const Person = denogres.Person;
-
-  // console.log("columns");
-  // console.log(denogres.Person.columns);
-
-  console.log(Person.columns);
-
-  const person = new denogres.Person();
-
-  console.log(Person.table);
-
-  interface Person2 {
-    _id: number;
-    name: string;
-    species_id: bigint;
-    current_mood: string;
-  }
-
-  const people: Person2[] = [
+  const people = [
     {
       name: "john",
       _id: 10,
-      current_mood: "happy",
       species_id: BigInt(2),
     },
     {
       name: "david",
       _id: 12,
-      current_mood: "happy",
       species_id: BigInt(2),
     },
     {
       name: "jessica",
       _id: 13,
-      current_mood: "happy",
       species_id: BigInt(2),
     },
   ];
 
-  let insertQuery = `INSERT INTO people `;
-  let columns = `(`;
-  let values = `(`;
+  let insertQuery = `INSERT INTO ${denogres.Person.table} (${Object.keys(denogres.Person.columns)}) VALUES `;
+  // let columns = `(`;
+  let values = ``;
 
-  // for (const person of people) {
-  //   for (const column in person) {
-  //     columns += `${column}, `;
-  //     values += `${person[column]}, `;
-  //   }
+  console.log('insertQuery', insertQuery);
+
+  // for (const column in denogres.Person.columns) {
+  //   console.log('column in denogres.PErson.columns', column);
+  //   columns += `${column}, `;
   // }
+
+  for (const person of people) {
+    values += `(${Object.values(person)}), `;
+  }
+
+  // console.log(columns, values);
+
+  values = values.slice(0, values.length - 2) + ';';
+
+  console.log(values);
+
+  insertQuery += values;
+
+  console.log('final insertQuery', insertQuery);
+
 
   DisconnectDb(db);
 });
