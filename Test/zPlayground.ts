eceaf383d9684477dffc4f3eca20801ca9b9da06
Henry Park <codedenma@gmail.com>
2022-09-17 15:49:26 -0700

    1.0.10

--- Test/zPlayground.ts
+++ Test/zPlayground.ts
@@ -1,19 +1,39 @@
 import { dbPull2 } from "../src/functions/dbPull2.ts";
 import { init } from "../src/functions/init.ts";
 import { resolve } from "https://deno.land/std@0.141.0/path/win32.ts";
-import {checkDbPull} from "../src/functions/checkDbPull.ts";
-import sync2 from "../src/functions/sync2.ts"
-import seed from "../src/functions/seed.ts"
-import {checkDbSync} from "../src/functions/checkDbSync.ts"
+import { checkDbPull } from "../src/functions/checkDbPull.ts";
+import sync2 from "../src/functions/sync2.ts";
+import seed from "../src/functions/seed.ts";
+import { checkDbSync } from "../src/functions/checkDbSync.ts";
+
+// const myLog = Deno.readFileSync(
+//   resolve("./Migrations/log/migration_log.txt"),
+// );
+// console.log(myLog);
+
+// const path =  "./Migrations/log/migration_log.txt";
+// console.log(path);
+
+// const myLog = Deno.readTextFileSync(path);
+// console.log(myLog);
+
+const myLog = Deno.readTextFileSync(
+  "./Migrations/log/migration_log.txt",
+);
+console.log(myLog);
+
+// const myLog = Deno.readFileSync(
+//   resolve("./Migrations/log/migration_log.txt"),
+// );
+// console.log(myLog);
 
 // await checkDbPull();
 
-sync2();
+// sync2();
 
 // await seed();
 
 // checkDbSync()
-
 
 // await dbPull2();
 
--- src/functions/init.ts
+++ src/functions/init.ts
@@ -2,9 +2,9 @@
 import { ensureDir } from "../../deps.ts";
 
 export function init() {
-// create .env file in root directory 
-    const envFilePath = "./";
-    const envFileContent = `
+  // create .env file in root directory
+  const envFilePath = "./";
+  const envFileContent = `
 # Set your environment either 'development' or 'test'
 ENVIRONMENT=development
 # Please enter your database uri below :
@@ -12,27 +12,28 @@ ENVIRONMENT=development
 DATABASE_URI=" "
 # (for test mode)
 TEST_DB_URI=" "
-`
-    // Check if root exists, then returns a promise wheich writes the file path, and the file content to the file path.
+`;
+  // Check if root exists, then returns a promise wheich writes the file path, and the file content to the file path.
 
-    ensureDir(envFilePath).then(() => {
+  ensureDir(envFilePath).then(() => {
     Deno.writeTextFile(envFilePath + ".env", envFileContent);
     // + add .env in gitignor file (if no gitignore file, make one)
-    console.log('.env file created')
-    })
+    console.log(".env file created");
+  });
 
-    const migrationFilePath = "./Migrations/log/";
-    ensureDir(migrationFilePath)
+  const migrationFilePath = "./Migrations/log/";
+  ensureDir(migrationFilePath);
+  console.log("Migrations folder created");
 
-// create moodel folder in root directory
-// inside the model folder, create model.ts file with boilerplate code
-    const modelFilePath = "./models/";
-    const modelFileContent = `    
+  // create moodel folder in root directory
+  // inside the model folder, create model.ts file with boilerplate code
+  const modelFilePath = "./models/";
+  const modelFileContent = `    
 import { Model } from 'https://deno.land/x/denogresdev/mod.ts'
 // user model definition comes here    
-    `
-    ensureDir(modelFilePath).then(() => {
+    `;
+  ensureDir(modelFilePath).then(() => {
     Deno.writeTextFile(modelFilePath + "model.ts", modelFileContent);
-    console.log('model.ts file created under model folder')
-    })
+    console.log("model.ts file created under model folder");
+  });
 }
