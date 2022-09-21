ce2c87353d083a478d347e7ce61fe05ae20a11a2
Henry Park <codedenma@gmail.com>
2022-09-21 10:39:58 -0700

    restore done

--- src/functions/restore.ts
+++ src/functions/restore.ts
@@ -2,7 +2,7 @@
 
 import { readLines } from "https://deno.land/std@0.141.0/io/buffer.ts";
 // import { promptString } from "./myLog.ts";
-import { resolve } from "https://deno.land/std@0.155.0/path/win32.ts";
+// import { resolve } from "https://deno.land/std@0.155.0/path/win32.ts";
 import sync from "./sync.ts";
 
 function isString(filePath: string | null): filePath is string {
@@ -15,26 +15,6 @@ function isString(filePath: string | null): filePath is string {
  * * (3): Db sync will then be ran.
  * * (4): If no is entered as decision, nothing will happen, function will exit.
  */
-// export async function restoreModel() {
-//   const filePath: string | null = prompt("Enter path of model to restore: ");
-//   //* check if current file path exists
-//   try {
-//     const restoreModel = Deno.readTextFileSync(filePath);
-//     const decision = await promptString(
-//       `This will overwrite your current model, are you sure you want to restore? \n Enter[yes/no]: `,
-//     );
-//     if (decision === "Yes" || decision === "Y" || decision === "yes") {
-//       console.log(restoreModel);
-//       //* Restore the model and dbSync.
-//       Deno.writeFileSync(resolve("./models/model.ts"), restoreModel); // resolve file paths just in case
-//       sync();
-//     }
-//   } catch (e) {
-//     if (e instanceof Deno.errors.NotFound) {
-//       console.error("file does not exists");
-//     }
-//   }
-// }
 export default function restoreModel() {
   let filePath: string | null = prompt("Enter path of model to restore: ");
   //* check if current file path exists
@@ -42,9 +22,13 @@ export default function restoreModel() {
   if (isString(filePath)) {
     // filePath = resolve(filePath?.replace(/\s/g, ""), "/build_model.ts");
     // filePath = resolve(filePath, "/build_model.ts");
-    filePath = filePath + "/build_model.ts";
+    if (filePath.includes("modelBuild")) {
+      filePath = filePath + "/build_model.ts";
+    } else if (filePath.includes("syncedModel")) {
+      filePath = filePath + "/synced_build.ts";
+    }
 
-    console.log("FILEPATH", filePath);
+    // console.log("FILEPATH", filePath);
     // filePath = resolve(filePath, "./build_model.ts");
     const restoreModel = Deno.readTextFileSync(filePath);
 
@@ -58,29 +42,18 @@ export default function restoreModel() {
       switch (decision) {
         case "yes":
         case "y": {
-          Deno.writeFileSync(filePath, restoreModel);
-          sync();
+          Deno.writeTextFileSync("./models/model.ts", restoreModel);
+
+          console.log("Syncing your database...");
+
+          sync(true);
           return;
+        }
+        case "no": {
+          console.log("Exiting restoration");
         }
         default:
       }
     } else return;
   } else return;
-
-  // try {
-  //   const restoreModel = Deno.readTextFileSync(filePath);
-  //   const decision = await promptString(
-  //     `This will overwrite your current model, are you sure you want to restore? \n Enter[yes/no]: `,
-  //   );
-  //   if (decision === "Yes" || decision === "Y" || decision === "yes") {
-  //     console.log(restoreModel);
-  //     //* Restore the model and dbSync.
-  //     Deno.writeFileSync(resolve("./models/model.ts"), restoreModel); // resolve file paths just in case
-  //     sync();
-  //   }
-  // } catch (e) {
-  //   if (e instanceof Deno.errors.NotFound) {
-  //     console.error("file does not exists");
-  //   }
-  // }
 }
