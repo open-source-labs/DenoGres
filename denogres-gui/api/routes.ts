f44df86c991169df01b42230a4afb26b41019819
James Howat <97859901+jbhowat@users.noreply.github.com>
2022-12-03 13:51:56 -0700

    created start of signIn route, updated login component on front end to hit required endpoint
    Co-authored-by: Azal Adeel <113378115+azaladeel@users.noreply.github.com>
    Co-authored-by: James Howat <97859901+jbhowat@users.noreply.github.com>
    Co-authored-by: Joe Hynan <111455487+JoeH1020@users.noreply.github.com>
    Co-authored-by: Hao Ze Lin <87346439+LinHAO-1@users.noreply.github.com>

--- denogres-gui/api/routes.ts
+++ denogres-gui/api/routes.ts
@@ -2,16 +2,18 @@ import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import columnNames from "./controllers/columnNames.ts";
 import getTables from "./controllers/getTables.ts";
 import getConstraints from "./controllers/getConstraints.ts"
-import userTables from "./controllers/userTables.ts";
+import signIn from "./controllers/signIn.ts";
 
 const router = new Router();
 
 router
+  // working db routes:
   .get('/api/tables', getTables)
   .get('/api/constraints', getConstraints)
   .get('/api/columns/:table', columnNames)
   
-  .get('/users/tables', userTables)
+  // user db routes:
+  .post('api/signIn', signIn)
 
 export default router;
 
