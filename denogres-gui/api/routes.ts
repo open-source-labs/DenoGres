b3d90b57ccd6b50496c0c76faf65a8bd6e7ae0dd
James Howat <97859901+jbhowat@users.noreply.github.com>
2022-12-08 08:55:55 -0700

    added test route at /api/test that will return whatever your request body payload was, also added a dev-serve deno task that runs the server using denon, which allows for hot reloading so you don't wear out your CTRL-C keys
    Co-authored-by: Azal Adeel <113378115+azaladeel@users.noreply.github.com>
    Co-authored-by: James Howat <97859901+jbhowat@users.noreply.github.com>
    Co-authored-by: Joe Hynan <111455487+JoeH1020@users.noreply.github.com>
    Co-authored-by: Hao Ze Lin <87346439+LinHAO-1@users.noreply.github.com>

--- denogres-gui/api/routes.ts
+++ denogres-gui/api/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { Context, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import columnNames from "./controllers/columnNames.ts";
 import getTables from "./controllers/getTables.ts";
 import getConstraints from "./controllers/getConstraints.ts"
@@ -13,7 +13,17 @@ router
   .get('/api/columns/:table', columnNames)
   
   // user db routes:
-  .post('/api/signin', signIn);
+  .post('/api/signin', signIn)
+
+  //test route:
+  .post('/api/test', async (ctx) => {
+    if (!ctx.request.hasBody) {
+      ctx.throw(415);
+    }
+    const reqBody = await ctx.request.body().value;
+    ctx.response.status = 200;
+    ctx.response.body = reqBody;
+  })
 
 export default router;
 
