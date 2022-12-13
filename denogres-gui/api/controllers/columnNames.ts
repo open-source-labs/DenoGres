f8682e72811c897fc3bab74a975bf59bc4cddb9f
James Howat <97859901+jbhowat@users.noreply.github.com>
2022-12-13 09:05:02 -0700

    routes for the ERD are all working now in postman

--- denogres-gui/api/controllers/columnNames.ts
+++ denogres-gui/api/controllers/columnNames.ts
@@ -1,5 +1,6 @@
 import { columnNames } from "../repositories/dbRepo.ts";
 import { Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { getOneConnection } from "../repositories/userRepo.ts";
 
 interface ContextWithParams extends Context {
   params: {
@@ -10,6 +11,7 @@ interface ContextWithParams extends Context {
 export default async ({
   params,
   response,
+  cookies,
 }: ContextWithParams) => {
   const tableName = params.table;
 
@@ -18,8 +20,14 @@ export default async ({
     response.body = { msg: "Invalid table name" };
     return;
   }
-
-  const foundTable = await columnNames(tableName);
+  const connectionId: string = await cookies.get('connectionId');
+  const userId: string = await cookies.get('userId');
+  if (!connectionId) {
+    response.status = 401;
+    response.body = 'Please log in to view your queries';
+  }
+  const connection = await getOneConnection(userId, connectionId);
+  const foundTable = await columnNames(tableName, connection);
   if (!foundTable) {
     response.status = 404;
     response.body = { msg: `Table with name ${tableName} not found` };
@@ -28,3 +36,4 @@ export default async ({
 
   response.body = foundTable;
 };
+
