57b69c08bbcd5ec7fc5dbfc6ba49e599c826b4bd
James Howat <97859901+jbhowat@users.noreply.github.com>
2022-12-13 07:40:04 -0700

    dev merge

--- denogres-gui/api/controllers/getTables.ts
+++ denogres-gui/api/controllers/getTables.ts
@@ -1,6 +1,6 @@
 import { allTables } from "../repositories/dbRepo.ts";
 import { Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
-export default async ({ response }: Context) => {
-  response.body = await allTables();
+export default async (ctx: Context) => {
+  ctx.response.body = await allTables(ctx.request.body.value());
 };
