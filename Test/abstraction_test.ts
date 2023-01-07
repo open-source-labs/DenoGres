ced57c166ef7f34fa71c4eda7ad05639d1769437
Rachel Cheman <rachelcheman@gmail.com>
2023-01-07 12:27:01 -0500

    started testing on Insert Method.

--- Test/abstraction_test.ts
+++ Test/abstraction_test.ts
@@ -1,6 +1,17 @@
-import { assert, assertEquals, assertStrictEquals, assertThrows, beforeAll, beforeEach, afterEach, afterAll, describe, it } from './deps.ts'
+import {
+  assert,
+  assertEquals,
+  assertStrictEquals,
+  assertThrows,
+  beforeAll,
+  beforeEach,
+  afterEach,
+  afterAll,
+  describe,
+  it,
+} from '../deps.ts';
 import { ConnectDb, DisconnectDb } from '../src/functions/Db.ts';
-import { Model } from '../src/class/Model.ts'
+import { Model } from '../src/class/Model.ts';
 
 // test tables (beforeAll) https://dbschema.com/2019/04/14/postgresql-drop-all-tables/
 const before_all = `
@@ -42,220 +53,243 @@ ALTER TABLE users ADD team_id INT;
 INSERT INTO users (firstName, points, team_id, gender) VALUES('user_six', 60, 1, 'F');
 INSERT INTO users (firstName, points, team_id, gender) VALUES('user_seven', 70, 2, 'F');
 INSERT INTO users (firstName, points, team_id, gender) VALUES('user_eight', 80, 1, 'M');
-`
+`;
 // drop tables (afterAll)
 const after_all = `
 DROP TABLE users CASCADE;
 DROP TABLE teams CASCADE;
-`
+`;
 
 interface User {
-  id:string;
-  firstname:string;
-  lastname?:string;
+  id: string;
+  firstname: string;
+  lastname?: string;
   points?: number;
-  team_id:number;
+  team_id: number;
 }
 
 class User extends Model {
   static table = 'users';
   static columns = {
-    id: { type:'uuid', primaryKey: true },
-    firstName: { type:'string', notNull: true },
-    lastName: { type:'string', notNull: false },
-    points: { type:'number', notNull: false },
-    team_id: {type:'number', noNull: true}
-  }
+    id: { type: 'uuid', primaryKey: true },
+    firstName: { type: 'string', notNull: true },
+    lastName: { type: 'string', notNull: false },
+    points: { type: 'number', notNull: false },
+    team_id: { type: 'number', noNull: true },
+  };
 }
 interface Team {
-  id:number;
-  teamname:string;
-  teamrole?:string;
+  id: number;
+  teamname: string;
+  teamrole?: string;
 }
 class Team extends Model {
   static table = 'teams';
   static columns = {
-    id: { type:'number', primaryKey: true },
-    teamname: { type:'string', notNull: true },
-    teamrole: { type:'string', notNull: false },
-  }
+    id: { type: 'number', primaryKey: true },
+    teamname: { type: 'string', notNull: true },
+    teamrole: { type: 'string', notNull: false },
+  };
 }
-interface IObj { // work-around
+interface IObj {
+  // work-around
   //firstName: unknown[];
-  [key:string]:any
+  [key: string]: any;
 }
-
 
 describe('Abstraction Test', () => {
   beforeAll(async () => {
-    if(Deno.env.get('ENVIRONMENT')!=='test') throw new Error('Not in test environment')
-    const db = await ConnectDb(); 
-    await db.queryObject(before_all) // executes the query defined above to set up the test db
-    await DisconnectDb(db)
-  })
+    if (Deno.env.get('ENVIRONMENT') !== 'test')
+      throw new Error('Not in test environment');
+    const db = await ConnectDb();
+    await db.queryObject(before_all); // executes the query defined above to set up the test db
+    await DisconnectDb(db);
+  });
 
   afterAll(async () => {
-    const db = await ConnectDb(); 
-    await db.queryObject(after_all)
-    await DisconnectDb(db)
-  })
+    const db = await ConnectDb();
+    await db.queryObject(after_all);
+    await DisconnectDb(db);
+  });
 
   // 1. saving 'Test' user
   it('save the record', async () => {
     const user0 = new User();
-    user0.firstname = 'Test'
-    const saved = await user0.save()
-    assertStrictEquals(saved.firstname, user0.firstname)
-  })
+    user0.firstname = 'Test';
+    const saved = await user0.save();
+    assertStrictEquals(saved.firstname, user0.firstname);
+  });
 
   // 2. saving a user 'Deno' and updating it to 'Deno updated'
-  it('update the record', async () =>{
+  it('update the record', async () => {
     let user0 = new User();
-    user0.firstname = "Deno"
-    const saved = await user0.save()
-    assertStrictEquals(saved.firstname, user0.firstname)
+    user0.firstname = 'Deno';
+    const saved = await user0.save();
+    assertStrictEquals(saved.firstname, user0.firstname);
 
-    user0.firstname = 'Deno Updated'
-    user0.lastname = 'Deno Land'
+    user0.firstname = 'Deno Updated';
+    user0.lastname = 'Deno Land';
 
-    const updatedModel = await user0.update()
-    assertStrictEquals(updatedModel.firstname, user0.firstname)
-    assertStrictEquals(updatedModel.lastname, user0.lastname)
+    const updatedModel = await user0.update();
+    assertStrictEquals(updatedModel.firstname, user0.firstname);
+    assertStrictEquals(updatedModel.lastname, user0.lastname);
 
-    const db = await ConnectDb(); 
-    const q1 = await db.queryObject(`SELECT * FROM users WHERE firstname = '${user0.firstname}'`)
-    await DisconnectDb(db)
+    const db = await ConnectDb();
+    const q1 = await db.queryObject(
+      `SELECT * FROM users WHERE firstname = '${user0.firstname}'`
+    );
+    await DisconnectDb(db);
 
-    const updatedConfrim = Object.assign(new User(), q1.rows[0])
-    assertStrictEquals(updatedConfrim.firstname, user0.firstname)
-    assertStrictEquals(updatedConfrim.lastname, user0.lastname)
-  })
+    const updatedConfrim = Object.assign(new User(), q1.rows[0]);
+    assertStrictEquals(updatedConfrim.firstname, user0.firstname);
+    assertStrictEquals(updatedConfrim.lastname, user0.lastname);
+  });
 
   // 3. inserting a new record 'temp'
-  it('insert column/value to the record', async ()=>{
-    await User.insert('firstname = temp', 'lastname = temp').query()
+  it('insert column/value to the record', async () => {
+    await User.insert('firstname = temp', 'lastname = temp').query();
 
     // bring the saved record from db
-    const db = await ConnectDb(); 
-    const q1 = await db.queryObject(`SELECT * FROM users WHERE firstname = 'temp'`)
-    await DisconnectDb(db)
+    const db = await ConnectDb();
+    const q1 = await db.queryObject(
+      `SELECT * FROM users WHERE firstname = 'temp'`
+    );
+    await DisconnectDb(db);
 
-    const inserted = Object.assign(new User(), q1.rows[0])
-    assertStrictEquals(inserted.firstname, 'temp')
-    assertStrictEquals(inserted.lastname, 'temp')
-  })
+    const inserted = Object.assign(new User(), q1.rows[0]);
+    assertStrictEquals(inserted.firstname, 'temp');
+    assertStrictEquals(inserted.lastname, 'temp');
+  });
 
   // 4. edit 'Test' --> 'Deno Land'
   it('edit the existing record', async () => {
     // edit must be used after where, otherwise it will update whole records in the table
-    await User.edit('firstname = Deno', 'lastname = Land').where('firstname = Test').query()
+    await User.edit('firstname = Deno', 'lastname = Land')
+      .where('firstname = Test')
+      .query();
 
     // bring the editted record from db
-    const db = await ConnectDb(); 
-    const q1 = await db.queryObject(`SELECT * FROM users WHERE firstname = 'Deno'`)
-    await DisconnectDb(db)
+    const db = await ConnectDb();
+    const q1 = await db.queryObject(
+      `SELECT * FROM users WHERE firstname = 'Deno'`
+    );
+    await DisconnectDb(db);
 
-    const editted = Object.assign(new User(), q1.rows[0])
-    assertStrictEquals(editted.firstname, 'Deno')
-    assertStrictEquals(editted.lastname, 'Land')   
-  })
+    const editted = Object.assign(new User(), q1.rows[0]);
+    assertStrictEquals(editted.firstname, 'Deno');
+    assertStrictEquals(editted.lastname, 'Land');
+  });
 
   // 5. deleting 'Temp'
   it('delete the record', async () => {
-    await User.delete().where('firstname = temp').query()
-    await User.delete().where('firstname = Deno').query()
-    await User.delete().where('firstname = Deno Updated').query()
-    
+    await User.delete().where('firstname = temp').query();
+    await User.delete().where('firstname = Deno').query();
+    await User.delete().where('firstname = Deno Updated').query();
 
     // bring the editted record from db
-    const db = await ConnectDb(); 
-    const q1 = await db.queryObject(`SELECT * FROM users WHERE firstname = 'temp'`)
-    await DisconnectDb(db)
-    assertEquals(q1.rows[0], undefined)
-  })
+    const db = await ConnectDb();
+    const q1 = await db.queryObject(
+      `SELECT * FROM users WHERE firstname = 'temp'`
+    );
+    await DisconnectDb(db);
+    assertEquals(q1.rows[0], undefined);
+  });
 
-  // 6. selecting all / and part of existing users 
-  it('select the record', async () =>{
-    const selected = await User.select('firstname, lastname').query() as IObj
-    assertEquals(selected.length, 8)
-  })
+  // 6. selecting all / and part of existing users
+  it('select the record', async () => {
+    const selected = (await User.select('firstname, lastname').query()) as IObj;
+    assertEquals(selected.length, 8);
+  });
 
   // 7. limit
   it('limit outcome of the record', async () => {
-    const limit = await User.select('firstname, lastname').limit(3).query()
-    assertEquals(limit.length, 3)
-  })
+    const limit = await User.select('firstname, lastname').limit(3).query();
+    assertEquals(limit.length, 3);
+  });
 
   // 8. innerJoin
-  it('selects records that have matching values from both tables', async()=>{
-    const innerjoin = await User.select('users.*').innerJoin('team_id', 'id', 'teams').where('points = 60').query() as IObj
+  it('selects records that have matching values from both tables', async () => {
+    const innerjoin = (await User.select('users.*')
+      .innerJoin('team_id', 'id', 'teams')
+      .where('points = 60')
+      .query()) as IObj;
     assertEquals(innerjoin[0].points, 60);
-  })
+  });
 
   // 9. leftJoin
-  it('selects records from first table and matching values on second table', async()=>{
-    const leftjoin = await User.select('users.*').leftJoin('team_id', 'id', 'teams').where('points = 60').query() as IObj
+  it('selects records from first table and matching values on second table', async () => {
+    const leftjoin = (await User.select('users.*')
+      .leftJoin('team_id', 'id', 'teams')
+      .where('points = 60')
+      .query()) as IObj;
     assertEquals(leftjoin[0].points, 60);
-  })
+  });
 
   // 10. rightJoin
-  it('selects records from first table and matching values on second table', async()=>{
-    const rightjoin = await User.select('users.*').rightJoin('team_id', 'id', 'teams').where('points = 60').query() as IObj
+  it('selects records from first table and matching values on second table', async () => {
+    const rightjoin = (await User.select('users.*')
+      .rightJoin('team_id', 'id', 'teams')
+      .where('points = 60')
+      .query()) as IObj;
     assertEquals(rightjoin[0].points, 60);
-  })
+  });
 
   // 11. fullJoin = outerJoin
-  it('selects all records when a match exists in either table', async()=>{
-    const fulljoin = await User.select('users.*').fullJoin('team_id', 'id', 'teams').where('points = 60').query() as IObj
+  it('selects all records when a match exists in either table', async () => {
+    const fulljoin = (await User.select('users.*')
+      .fullJoin('team_id', 'id', 'teams')
+      .where('points = 60')
+      .query()) as IObj;
     assertEquals(fulljoin[0].points, 60);
-  })
-  
+  });
+
   // 12. group is used with aggregation functions
-  it('group rows with same values', async() =>{
-    const group = await User.select('SUM(points), gender').group('gender').query()
-    assertEquals(group[0], { sum: 140n, gender: "M" })
-    assertEquals(group[1], { sum: 220n, gender: "F" }) 
-  })
+  it('group rows with same values', async () => {
+    const group = await User.select('SUM(points), gender')
+      .group('gender')
+      .query();
+    assertEquals(group[0], { sum: 140n, gender: 'M' });
+    assertEquals(group[1], { sum: 220n, gender: 'F' });
+  });
 
   // 13. order
-  it('sort column(s) by ascending or descending order', async() =>{
-    const order = await User.select('firstname, points').order('DESC', 'points').query() as IObj;
-    assertEquals(order[0].points, 80)
-    assertEquals(order[1].points, 70)
-    assertEquals(order[2].points, 60)
-  })
+  it('sort column(s) by ascending or descending order', async () => {
+    const order = (await User.select('firstname, points')
+      .order('DESC', 'points')
+      .query()) as IObj;
+    assertEquals(order[0].points, 80);
+    assertEquals(order[1].points, 70);
+    assertEquals(order[2].points, 60);
+  });
 
   // 14. count
-  it('calculate aggregate functions - COUNT', async() =>{
+  it('calculate aggregate functions - COUNT', async () => {
     const count = await User.count('id').query();
-    assertEquals(count[0], { count: 8n }) 
-  })
+    assertEquals(count[0], { count: 8n });
+  });
 
-  // 15. sum 
-  it('calculate aggregate functions - SUM', async() =>{
+  // 15. sum
+  it('calculate aggregate functions - SUM', async () => {
     const sum = await User.sum('points').query();
-    assertEquals(sum[0], { sum: 360n }) 
-  })
-
+    assertEquals(sum[0], { sum: 360n });
+  });
 
   // 16. avg
-  it('calculate aggregate functions - AVG', async() =>{
-    const avg = await User.avg('points').query() as IObj
-    assertEquals(Math.trunc(avg[0].avg), 45) 
-  })
+  it('calculate aggregate functions - AVG', async () => {
+    const avg = (await User.avg('points').query()) as IObj;
+    assertEquals(Math.trunc(avg[0].avg), 45);
+  });
   // 17. min
-  it('calculate aggregate functions - MIN', async() =>{
-    const min = await User.min('points').query() as IObj
-    assertEquals(min[0].min, 10) 
-  })
+  it('calculate aggregate functions - MIN', async () => {
+    const min = (await User.min('points').query()) as IObj;
+    assertEquals(min[0].min, 10);
+  });
   // // 18. max
-  it('calculate aggregate functions - MAX', async() =>{
-    const max = await User.max('points').query() as IObj
-    assertEquals(max[0].max, 80) 
-  })
-
+  it('calculate aggregate functions - MAX', async () => {
+    const max = (await User.max('points').query()) as IObj;
+    assertEquals(max[0].max, 80);
+  });
 
   // *** Methods that cannot have stand-alone test ***
   // 'where', 'having', 'query','queryInstance'
-
-})
+});
--- src/class/Model.md
+++ src/class/Model.md
@@ -5,13 +5,16 @@
 ### **Save**
 
 #### Expected Behavior:
+
 > when a user creates a new model instance, adds properties to it, and invokes the `save` method...
+>
 > - a query will be sent to the user's db adding a new row to the corresponding table with
-> those prop/value pairs (props representing column names)
+>   those prop/value pairs (props representing column names)
 > - the added row (returned by the query) will be stored on the record property of the model instance
 > - the model instance will be returned
 
 #### Edge Cases:
+
 > - the user invokes the save method without adding any properties
 > - the user invokes the save method without adding a property that is required
 > - the user invokes the save method after adding a property that does not correspond to a column name
@@ -22,14 +25,17 @@
 ### **Update**
 
 #### Expected Behavior:
+
 > when a user adds properties to a model instance they've already saved to the db and invokes `update`...
+>
 > - a query will be sent to the user's db targetting the row in the table represented by that instance
-> (as stored at the `record` property when the `save` method was invoked)
+>   (as stored at the `record` property when the `save` method was invoked)
 > - the query will update the row with the new key/value pairs added by the user
 > - the updated row (returned by the query) will be stored on the record property of the model instance
-> - the model instance will be returned 
+> - the model instance will be returned
 
 #### Edge Cases:
+
 > - the user invokes the update method on a model instance that hasn't been saved to the db already
 > - ^^^ same edge cases as for the save method
 
@@ -38,8 +44,10 @@
 ### **Insert**
 
 #### Expected Behavior:
+
 > when a user invokes the `insert` method on a model class and passes in one or more strings of the form
 > `column_name = value`...
+>
 > - a query will be stored at the `sql` property of the class
 > - query will be of the form `INSERT INTO table_name (column1, column2, ...) VALUES (value1, value2, ...)`
 > - when executed, query will add a new row to the db table with values in corresponding columns
@@ -47,6 +55,7 @@
 > - when chained with `query` method, returns empty array
 
 #### Edge Cases:
+
 > - the user invokes the insert method without any arguments
 > - the user invokes the insert method with malformatted arguments
 > - the user invokes the insert method with column names that don't exist on the table
@@ -55,45 +64,56 @@
 > - the user chains the insert method with incompatible methods
 
 #### Lacking Functionality:
+
 > - have to specify column names--can't just `INSERT INTO table_name VALUES (...)`
 > - can't insert multiple rows at once
 > - can't insert a row with all default values
 > - can't chain insert with the select method to insert rows from another table
+> - no error handling for no arguments
 
 ### **Delete**
 
 #### Expected Behavior:
+
 > when a user invokes the `delete` method on a model class (without any parameters)...
+>
 > - the query `DELETE FROM table_name` will be stored at the `sql` property of the class
 > - the method itself returns the model (with query string appended)
 > - when executed (with `query` method), will delete entire table associated with model and return empty array
 > - when chained with `where` method, will delete only rows where conditions are met
 
 #### Edge Cases:
+
 > - the user chains delete with incompatible methods
 
 ### **Select**
 
 #### Expected Behavior:
+
 > when a user invokes the `select` method on a model class with one or more column names passed in...
+>
 > - the query `SELECT column_name1, column_name2, ... FROM table_name` will be stored at the `sql` property of the class
 > - the method itself returns the model (with query string appended)
 > - when executed itself (with `query` method), will return those columns from all rows as an array of objects
 > - when chained with `where` method, will return only rows that meet conditions
 
 #### Edge Cases:
+
 > - user invokes without any arguments
 > - user invokes with columns that aren't in the table
 > - user chains with incompatible methods
 
 #### Lacking Functionality:
+
 > - user could theoretically use aliases with `AS` but no documentation/explicit support for it
 
 ### **Where**
 
 #### Expected Behavior:
+
 > when a user invokes the `where` method on a model class with one or more conditions of the form
 > `column_name = value`, `column_name > value`, or `column_name LIKE value`...
+>
 > - if not chained onto another method, the query `SELECT * FROM table_name` is stored at the `sql` property on the class
 > - conditions are split on the following characters: =, >, <, >=, <=, LIKE
 > - the second half of the condition is wrapped in single quotes
@@ -101,6 +121,7 @@
 > - the method itself returns the model (with query string appended)
 
 #### Edge Cases:
+
 > - the user invokes without any arguments
 > - the user invokes with malformatted conditions (or unsupported syntax)
 > - the user invokes with column names not on the table
@@ -108,6 +129,7 @@
 > - the user chains with incompatible methods
 
 #### Lacking Functionality:
+
 > - can't specify condition where column_name `IS NOT NULL`
 > - can't use `IN` keyword, ex: `WHERE column_name IN ( value1, value2, ... )`
 > - can't use `BETWEEN` keyword, ex: `WHERE column_name BETWEEN value1 AND value2`
@@ -117,11 +139,14 @@
 ### **Limit**
 
 #### Expected Behavior:
+
 > when a user invokes the `limit` method on a model class with a number...
+>
 > - the query string `LIMIT number` is appended to the `sql` property of the model class
 > - the model is returned (with query string appended)
 > - when executed along with a select query, will limit the number of rows returned
 
 #### Edge Cases:
+
 > - the user invokes without any argument
 > - the user invokes with the wrong type of argument
