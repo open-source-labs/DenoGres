3dc49cb12bc8892115b540b5fea525a1f8291dd1
Mia <mjpgussen@gmail.comn>
2023-01-10 12:42:03 -0500

    fixes another typo in test file

--- client.js
+++ client.js
@@ -12,12 +12,12 @@ import { Pool } from './deps.ts';
 const pool = new Pool(
   'postgres://ynhufyow:cUTssLTW2NFX97XvQUjgX1r-OiUi924y@berry.db.elephantsql.com/ynhufyow',
   3,
-  true,
+  true
 );
 
 // pgclient.connect();
 
-pgclient = pool.connect();
+const pgclient = pool.connect();
 
 const table =
   'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))';
