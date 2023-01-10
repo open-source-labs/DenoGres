99d297119492b2e6009341d425854bea081af755
Mia <mjpgussen@gmail.comn>
2023-01-10 12:34:03 -0500

    switch from client to pool connection in test file

--- client.js
+++ client.js
@@ -1,14 +1,23 @@
-import { Client } from './deps.ts';
+// import { Client } from './deps.ts';
+import { Pool } from './deps.ts';
 
-const pgclient = new Client({
-  hostname: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
-});
+// const pgclient = new Client({
+//   hostname: 'localhost',
+//   port: 5432,
+//   user: 'postgres',
+//   password: 'postgres',
+//   database: 'postgres',
+// });
 
-pgclient.connect();
+const pool = new Pool(
+  'postgres://ynhufyow:cUTssLTW2NFX97XvQUjgX1r-OiUi924y@berry.db.elephantsql.com/ynhufyow',
+  3,
+  true
+);
+
+// pgclient.connect();
+
+pool.connect();
 
 const table =
   'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))';
@@ -22,16 +31,30 @@ const values = [
   'octocat@github.com',
 ];
 
-pgclient.queryObject(table, (err) => {
+// pgclient.queryObject(table, (err) => {
+//   if (err) throw err;
+// });
+
+pool.queryObject(table, (err) => {
   if (err) throw err;
 });
 
-pgclient.queryObject(text, values, (err) => {
+// pgclient.queryObject(text, values, (err) => {
+//   if (err) throw err;
+// });
+
+pool.queryObject(text, values, (err) => {
   if (err) throw err;
 });
 
-pgclient.queryObject('SELECT * FROM student', (err, res) => {
+// pgclient.queryObject('SELECT * FROM student', (err, res) => {
+//   if (err) throw err;
+//   console.log(err, res.rows); // Print the data in student table
+//   pgclient.end();
+// });
+
+pool.queryObject('SELECT * FROM student', (err, res) => {
   if (err) throw err;
   console.log(err, res.rows); // Print the data in student table
-  pgclient.end();
+  pool.end();
 });
