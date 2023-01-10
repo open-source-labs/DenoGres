2049a6c5dbd8e6dedebe9e36b14a5e2a4ae134bb
Mia <mjpgussen@gmail.comn>
2023-01-10 11:53:27 -0500

    format dep.ts

--- deps.ts
+++ deps.ts
@@ -2,7 +2,11 @@
 
 export { config, parse } from 'https://deno.land/std@0.148.0/dotenv/mod.ts'; // mod.ts
 
-export { Pool, PoolClient, Client } from 'https://deno.land/x/postgres@v0.17.0/mod.ts'; // Db.ts
+export {
+  Client,
+  Pool,
+  PoolClient,
+} from 'https://deno.land/x/postgres@v0.17.0/mod.ts'; // Db.ts
 
 export { ensureDir } from 'https://deno.land/std@0.165.0/fs/ensure_dir.ts'; // init.ts, checkDbPull.ts, checkDbSync.ts
 
