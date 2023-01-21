332f78c1bea10ceb88894c5c3007044fd601c78e
Rachel Cheman <rachelcheman@gmail.com>
2023-01-21 16:25:20 -0500

    Adds association integration tests for hasOne method.

--- Test/association_integration_test.ts
+++ Test/association_integration_test.ts
@@ -11,7 +11,14 @@ import {
   createTablesQuery,
   dropTablesQuery,
 } from './model_integration_tests/seed_testdb.ts';
-import { Person, Planet, Species, Vessel } from './sample_model.ts';
+import {
+  Person,
+  Pilot,
+  Planet,
+  Species,
+  StarshipSpec,
+  Vessel,
+} from './sample_model.ts';
 import { getMappingKeys } from '../src/class/Model.ts';
 import 'https://deno.land/x/dotenv@v3.2.0/load.ts';
 
@@ -29,7 +36,6 @@ describe('association methods', () => {
     await db.release();
     await pool.end();
   });
-
   describe('getMappingKeys helper function', () => {
     it('returns the corresponding mapped keys between two tables', async () => {
       const mappedKeys = await getMappingKeys(Person.table, Species.table);
@@ -41,12 +47,25 @@ describe('association methods', () => {
       };
       assertEquals(mappedKeys, expectedKeys);
     });
-    it('returns undefined if there is no establish relationship between the tables', async () => {
+    it('returns undefined if there is no established relationship between the tables', async () => {
       const mappedKeys = await getMappingKeys(Person.table, Vessel.table);
       assert(mappedKeys === undefined);
     });
   });
   describe('belongsTo method', () => {
+    // reset the database to be able to make new associations
+    let pool: Pool;
+    let db: PoolClient;
+    beforeAll(async () => {
+      pool = new Pool(Deno.env.get('TEST_DB_URI'), 1);
+      db = await pool.connect();
+      await db.queryObject(createTablesQuery);
+    });
+    afterAll(async () => {
+      await db.queryObject(dropTablesQuery);
+      await db.release();
+      await pool.end();
+    });
     it('Adds a getter method with the appropriate name to the existing association', async () => {
       // NOTE: This will also console.log 'No association query exists.'
       // This is from the syncAssociation function in the Association.ts file
@@ -72,7 +91,7 @@ describe('association methods', () => {
       await personPlanet.syncAssociation();
       assert(personPlanet.associationQuery.length === 0);
     });
-    it('creates a new belongsTo association if it does not exist', async () => {
+    it('Creates a new belongsTo association if it does not exist', async () => {
       const vesselPersonAssociation = await Person.belongsTo(Vessel);
       await vesselPersonAssociation.syncAssociation();
       const mappedKeys = await getMappingKeys(Person.table, Vessel.table);
@@ -85,10 +104,46 @@ describe('association methods', () => {
       assertEquals(mappedKeys, expectedKeys);
     });
   });
+  describe('hasOne method', () => {
+    // reset the database to be able to make new associations
+    let pool: Pool;
+    let db: PoolClient;
+    beforeAll(async () => {
+      pool = new Pool(Deno.env.get('TEST_DB_URI'), 1);
+      db = await pool.connect();
+      await db.queryObject(createTablesQuery);
+    });
+    afterAll(async () => {
+      await db.queryObject(dropTablesQuery);
+      await db.release();
+      await pool.end();
+    });
+
+    it('Adds getter method with the appropriate name on a new association', async () => {
+      const vesselSpecs = await Vessel.hasOne(StarshipSpec);
+      await vesselSpecs.syncAssociation();
+      const millenniumFalcon = await Vessel.select()
+        .where('name = Millennium Falcon')
+        .queryInstance();
+      assert(millenniumFalcon.getStarshipSpec);
+    });
+    it('Adds getter functionality to retrieve the associated row', async () => {
+      const millenniumFalcon = await Vessel.select()
+        .where('name = Millennium Falcon')
+        .queryInstance();
+      const millenniumSpecs = await millenniumFalcon.getStarshipSpec();
+      const expectedSpecs = {
+        _id: 4,
+        hyperdrive_rating: '0.5',
+        MGLT: '75',
+        vessel_id: 10n,
+      };
+      assertEquals(millenniumSpecs[0], expectedSpecs);
+    });
+  });
 });
 
 // Future Methods to test
-// hasOne
 
 // hasMany
 
