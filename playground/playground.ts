e86c81054a833c254625c780212be4f345052078
alexanderlin <alexraptordev@gmail.com>
2023-01-22 09:41:33 -0500

    check if these vendored dependencies are enough for denogres

--- playground/playground.ts
+++ playground/playground.ts
@@ -1,26 +1,26 @@
 import { Person } from '../models/model.ts';
 try {
-  await Person.insert('name = Elvin').transaction();
+  // await Person.insert('name = Elvin').transaction();
 
-  await Person.insert('name1 = Beef').transaction();
+  // await Person.insert('name = Beef').transaction();
 
-  await Person.insert('name = Sakura').transaction();
+  // await Person.insert('name = Sakura').transaction();
 
-  await Person.insert('name = Shoutout').transaction();
+  // await Person.insert('name = Shoutout').transaction();
 
-  await Person.endTransaction();
+  // await Person.endTransaction();
 } catch (e) {
   console.error(e);
 }
 try {
-  // await Person.delete().where('name = Sakura').transaction();
-  // await Person.delete().where('name = Beef').transaction();
+  await Person.delete().where('name = Sakura').transaction();
+  await Person.delete().where('name = Beef').transaction();
 
-  // await Person.delete().where('name = Elvin').transaction();
+  await Person.delete().where('name = Elvin').transaction();
 
-  // await Person.delete().where('name = Shoutout').transaction();
+  await Person.delete().where('name = Shoutout').transaction();
 
-  // await Person.endTransaction();
+  await Person.endTransaction();
 } catch (e) {
   console.error(e);
 }
