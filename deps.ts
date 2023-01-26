19b6729e3bf95a28e3433cb5204550de03bd10c6
Rachel Cheman <rachelcheman@gmail.com>
2023-01-26 10:42:47 -0500

    updates deno vendor to std library 173, deletes deno.lock file, updates version to 173 in deps.ts file.

--- deps.ts
+++ deps.ts
@@ -1,16 +1,16 @@
 /* This module should re-export all remote dependencies for use in other modules */
 
-export { config, parse } from 'https://deno.land/std@0.171.0/dotenv/mod.ts'; // mod.ts
+export { config, parse } from 'https://deno.land/std@0.173.0/dotenv/mod.ts'; // mod.ts
 
 export { Pool, PoolClient } from 'https://deno.land/x/postgres@v0.17.0/mod.ts'; // Db.ts
 
-export { ensureDir } from 'https://deno.land/std@0.171.0/fs/ensure_dir.ts'; // init.ts, checkDbPull.ts, checkDbSync.ts
+export { ensureDir } from 'https://deno.land/std@0.173.0/fs/ensure_dir.ts'; // init.ts, checkDbPull.ts, checkDbSync.ts
 
-export { resolve } from 'https://deno.land/std@0.170.0/path/mod.ts'; //checkDbSync.ts, dbPull.ts, modelParser.ts, seed.ts, checkDbPull.ts,
+export { resolve } from 'https://deno.land/std@0.173.0/path/mod.ts'; //checkDbSync.ts, dbPull.ts, modelParser.ts, seed.ts, checkDbPull.ts,
 
-export { join } from 'https://deno.land/std@0.171.0/path/win32.ts'; // introspect.ts
+export { join } from 'https://deno.land/std@0.173.0/path/win32.ts'; // introspect.ts
 
-export { readLines } from 'https://deno.land/std@0.171.0/io/buffer.ts'; // myLog.ts, restore.ts
+export { readLines } from 'https://deno.land/std@0.173.0/io/buffer.ts'; // myLog.ts, restore.ts
 
 export {
   assert,
@@ -22,7 +22,7 @@ export {
   assertRejects,
   assertStrictEquals,
   assertThrows,
-} from 'https://deno.land/std@0.171.0/testing/asserts.ts';
+} from 'https://deno.land/std@0.173.0/testing/asserts.ts';
 
 export {
   afterAll,
@@ -31,7 +31,7 @@ export {
   beforeEach,
   describe,
   it,
-} from 'https://deno.land/std@0.171.0/testing/bdd.ts';
+} from 'https://deno.land/std@0.173.0/testing/bdd.ts';
 
 // unknown how to export : import "https://deno.land/x/dotenv@v3.2.0/load.ts"; which is imported on files: ./mod.ts, ./src/Db.ts, ./Test/dbPing.ts, ./Test/association.ts
 
